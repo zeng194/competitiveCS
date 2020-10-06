be05ab4fe4c5cbef46d8cd41016a0af4d37d53e8
zeng194 <zeng194@purdue.edu>
2020-10-06 15:21:27 -0400

    Create sqrtX.java

---
+++ sqrtX.java
@@ -0,0 +1,169 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class sqrtX {
+
+    public static void main(String[] args) throws IOException{
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        double n = in.nextDouble();
+        double half = 1/2;
+        Long index = Math.round(search(n)-half);
+        out.println(index);
+        out.close();
+    }
+    private static double search(double n) {
+        double first = 1;
+        double last = Math.round(n/2) + 1;
+        double mid = Math.round((first+last)/2);
+        System.out.println(first + " " + mid + " " + last);
+        while(first <= last){
+            double square = mid * mid;
+            if (square < n){  
+                double square2 = (mid + 1) * (mid + 1);
+                if (square2 > n) {
+                    return mid;
+                }
+                else {
+                    first = mid + 1; 
+                }
+            }
+            else if (square > n){
+                System.out.println("mid: " + mid + " square: " + square);
+                double square3 = (mid-1) * (mid-1);
+                if (square3 < n) {
+                    return (mid - 1);
+                }
+                else {
+                    last = mid - 1;
+                }
+            }
+            else{  
+                return mid; 
+            }  
+            mid = Math.round((first + last)/2);  
+        }  
+        return 0;
+    }
+    static class FastReader { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private int MAX_LINE_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer, lineBuf; 
+        private int bufferPointer, bytesRead; 
+  
+        public FastReader() { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            lineBuf = new byte[MAX_LINE_SIZE];
+            bufferPointer = bytesRead = 0; 
+        } 
+        public FastReader(String file_name) throws IOException { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE];  
+            bufferPointer = bytesRead = 0; 
+        } 
+        public boolean hasNext() throws IOException { 
+            byte c; 
+            while ((c = read()) != -1) { 
+                if (c > ' ') {        // Find first byte bigger than ' '
+                    bufferPointer--;
+                    return true;
+                }
+            }
+            return false;
+        }
+        // return the next line that contains at least one character > ' '
+        public String nextLine() throws IOException { 
+            int ctr = 0;
+            byte c; 
+            boolean empty = true;
+            while ((c = read()) != -1) { 
+                if (c == '\r')        continue;     // ignore '\r'
+                if (c == '\n') {
+                    if (empty)  { ctr = 0;   continue;  } // read only spaces etc. until \n
+                    else        break;
+                }
+                if (ctr == MAX_LINE_SIZE) {
+                    MAX_LINE_SIZE *= 2;
+                    lineBuf = Arrays.copyOf(lineBuf, MAX_LINE_SIZE);
+                }
+                lineBuf[ctr++] = c; 
+                if (c > ' ')  empty = false;
+            } 
+            return new String(lineBuf, 0, ctr); 
+        }
+        public String next() throws IOException { 
+            int ctr = 0;
+            byte c = read(); 
+            while (c <= ' ')      c = read(); 
+            while (c > ' ') {
+                if (ctr == MAX_LINE_SIZE) {
+                    MAX_LINE_SIZE *= 2;
+                    lineBuf = Arrays.copyOf(lineBuf, MAX_LINE_SIZE);
+                }
+                lineBuf[ctr++] = c;
+                c = read();
+            }
+            return new String(lineBuf, 0, ctr);
+        }
+        public int nextInt() throws IOException { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ')   c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg)           c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (neg)           return -ret; 
+            return ret; 
+        } 
+        public long nextLong() throws IOException { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ')    c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg)            c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } while ((c = read()) >= '0' && c <= '9'); 
+            if (neg)            return -ret; 
+            return ret; 
+        } 
+        public double nextDouble() throws IOException { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg)      c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (c == '.') { 
+                while ((c = read()) >= '0' && c <= '9') { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+            if (neg)     return -ret; 
+            return ret; 
+        } 
+        private void fillBuffer() throws IOException { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+        } 
+        private byte read() throws IOException { 
+            if (bufferPointer == bytesRead)     fillBuffer(); 
+            if (bytesRead <= 0)  return -1;  // No data
+            return buffer[bufferPointer++]; 
+        } 
+        public void close() throws IOException { 
+            if (din == null)        return; 
+            din.close(); 
+        } 
+    } 
+}
